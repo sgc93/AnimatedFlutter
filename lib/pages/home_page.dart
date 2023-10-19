2b6baf15d33eebf57d78d15261c30f9991186070
sgc93 <smachewgedefaw@gmail.com>
2023-10-18 23:58:35 -0900

    apply functional programming

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,11 +11,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade700,
       appBar: AppBar(
-        title: const Row(
-          children: [Icon(Icons.pets_sharp), Text('Cute Cat | Animation')],
-        ),
+        backgroundColor: Colors.grey.shade700,
+        elevation: 0,
+        title: appBarTitle(),
+      ),
+      body: animationContainer(),
+    );
+  }
+
+  Widget appBarTitle() {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.pets_sharp),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Cute Cat | Animation'),
+          ),
+        ],
       ),
     );
+  }
+
+  animationContainer() {
+    return Image.asset('assets/images/cat1.jpg');
   }
 }
